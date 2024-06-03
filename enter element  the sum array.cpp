5a55bf4fd6d8853196fdbf266e0dc335d8ca32eb
ANKIT KUMAR JHA <141003065+ankit0912k@users.noreply.github.com>
2024-06-03 23:11:12 +0530

    Add files via upload

---
+++ enter element  the sum array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int arr[4]={2,5,3,1};
+    
+    int sum=0;
+    for(int i=0;i<4;i++){
+    
+    sum=sum+arr[i];
+    }
+    cout<<"enter the sum element in array:"<<sum<<endl;
+    
+    return 0;
+}
+
+
